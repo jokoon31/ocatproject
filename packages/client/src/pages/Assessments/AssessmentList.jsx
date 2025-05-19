9ab22484473fc726c6e98ead1d2df9701b309f37
jokoon31 <jokoon15@outlook.com>
2025-05-19 09:13:25 -0400

    Finished?2

--- packages/client/src/pages/Assessments/AssessmentList.jsx
+++ packages/client/src/pages/Assessments/AssessmentList.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import {
   createColumnHelper,
   flexRender,
-  getColumnFilteredRowModel,
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
